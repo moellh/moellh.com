370f6928c34638112eb55f7c20ed1f9b8b4fe42a
Henrik Möllmann <henmoell@gmail.com>
2025-02-02 14:56:46 +0100

    Fixed order of operations, Removed reinstall.sh

--- reinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "=== Reinstalling moellh.com ==="
-
-SERVER_DIR=/var/www/html
-rsync -av --delete website/ "$SERVER_DIR"
--- update.sh
+++ update.sh
@@ -6,7 +6,10 @@ git fetch origin
 
 if ! git diff --quiet origin/main; then
   git pull origin main
-  ./reinstall.sh
   echo "Update up to commit with string: $(git log -1 --pretty=format:"%h %s")"
+
   journalctl -u moellh.com.service > $HOME/dev/moellh.com/website/journalctl.log
+
+  SERVER_DIR=/var/www/html
+  rsync -av --delete website/ "$SERVER_DIR"
 fi
