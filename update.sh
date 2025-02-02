abb11a9d339d32bcd90da2367f019caa65b5ca94
Henrik Möllmann <henmoell@gmail.com>
2025-02-02 14:47:33 +0100

    Added website/journalctl.log for online inspection of status

--- update.sh
+++ update.sh
@@ -8,4 +8,5 @@ if ! git diff --quiet origin/main; then
   git pull origin main
   ./reinstall.sh
   echo "Update up to commit with string: $(git log -1 --pretty=format:"%h %s")"
+  journalctl -u moellh.com.service > $HOME/dev/moellh.com/website/journalctl.log
 fi
