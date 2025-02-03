1eaa3ac75dfd2cd0f5f0ab6d70f0468c76424ca8
Henrik Möllmann <henmoell@gmail.com>
2025-02-03 10:40:06 +0100

    Changed journalctl.log to update on each invocation of script

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ if ! git diff --quiet origin/main; then
   git pull origin main
   echo "Update up to commit: $(git log -1 --pretty=format:"%h %s")"
 
-  journalctl -u moellh-com-update.service > $HOME/dev/moellh.com/website/journalctl.log
-
   SERVER_DIR=/var/www/html
   rsync -av --delete website/ "$SERVER_DIR"
 fi
+
+journalctl -u moellh-com-update.service > $HOME/dev/moellh.com/website/journalctl.log
