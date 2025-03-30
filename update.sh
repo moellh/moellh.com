508a94b1ff2da91d3ba18b6ed2369a9bbc064bc7
Henrik Möllmann <henmoell@gmail.com>
2025-03-30 15:15:07 +0200

    feat(submodule): add MensaKrabbler for automated index.html updates
    Integrated the MensaKrabbler repository as a submodule to automatically
    update `mensakrabbler/index.html`.

--- update.sh
+++ update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd $HOME/dev/moellh.com/
-
 git fetch origin
+git submodule update --init --recursive
 
 if ! git diff --quiet origin/main; then
   git pull origin main
   echo "Update up to commit: $(git log -1 --pretty=format:"%h %s")"
-
-  SERVER_DIR=/var/www/html
-  rsync -av --delete website/ "$SERVER_DIR"
 fi
 
-journalctl -u moellh-com-update.service > $HOME/dev/moellh.com/website/journalctl.log
+./mensakrabbler/update.sh
+cp ./mensakrabbler/website/index.html ./website/mensakrabbler/index.html
+
+journalctl -u moellh-com-update.service > ./website/journalctl.log
+
+SERVER_DIR=/var/www/html
+rsync -av --delete website/ "$SERVER_DIR"
